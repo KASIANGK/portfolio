00747bf7fd515a5d76d77322befec21a622913a1
KASIANGK <ngk.kasia@gmail.com>
2026-01-22 20:37:13 +0100

    HomeOverlay, MenuPreview, StepMenu - data, cache and style - saved

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,20 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import "./i18n";
+import React from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import i18n from "./i18n"; // IMPORTANT: exporte ton instance i18n dans ce fichier
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+async function bootstrap() {
+  // Charge ce que tu veux AVANT le premier render
+  await i18n.loadNamespaces(["intro", "nav"]);
+  // Si tu as une liste fixe de langues:
+  await i18n.loadLanguages(["en", "fr", "pl", "nl"]); // adapte à tes LANGS
+
+  createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+}
+
+bootstrap();
