e22412a9707b1f4beafc82848f5d69c3205193e2
KASIANGK <ngk.kasia@gmail.com>
2026-01-22 16:34:08 +0100

    HomeOverlay.jsx - new arborescence_updated

---
+++ src/Components/Home/HomeOverlay/parts/OverlayResetButtons.jsx
@@ -0,0 +1,23 @@
+// src/Components/Home/HomeOverlay/parts/OverlayResetButtons.jsx
+import React from "react";
+
+export default function OverlayResetButtons({ t, onResetLanguage, onResetHint, onResetStep }) {
+  return (
+    <>
+      <button type="button" className="homeOverlay__resetBtn" onClick={onResetLanguage}>
+        <span className="homeOverlay__resetIcon" aria-hidden="true">↺</span>
+        <span className="homeOverlay__resetText">{t("language.reset")}</span>
+      </button>
+
+      <button type="button" className="homeOverlay__resetBtn__Hint" onClick={onResetHint}>
+        <span className="homeOverlay__resetIcon" aria-hidden="true">↺</span>
+        <span className="homeOverlay__resetText">RESET HINT</span>
+      </button>
+
+      <button type="button" className="homeOverlay__resetBtn__Step" onClick={onResetStep}>
+        <span className="homeOverlay__resetIcon" aria-hidden="true">↺</span>
+        <span className="homeOverlay__resetText">RESET STEP1</span>
+      </button>
+    </>
+  );
+}
